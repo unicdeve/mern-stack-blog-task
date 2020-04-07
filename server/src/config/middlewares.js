55c02c47471082f7c0433904ebfb3f6ce510c700
Taiwo Ogunola <uniqueomokenny@gmail.com>
2020-04-07 23:21:38 +0100

    PWA setup done, express compression and HTTPS enforce

--- server/src/config/middlewares.js
+++ server/src/config/middlewares.js
@@ -2,6 +2,8 @@ import morgan from 'morgan';
 import express from 'express';
 import passport from 'passport';
 import cors from 'cors';
+import compression from 'compression';
+import enforce from 'express-sslify';
 
 import passportConfig from './check-auth';
 
@@ -9,8 +11,10 @@ import { isDev } from '../constants';
 
 export default app => {
   app.use(morgan(isDev ? 'dev' : 'common'));
+  app.use(compression());
   app.use(express.json());
   app.use(passport.initialize());
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(cors());
   passportConfig(passport);
 };
--- server/src/server.js
+++ server/src/server.js
@@ -13,6 +13,18 @@ middlewaresConfig(app);
 app.use('/api/v1/users', UserRoutes);
 app.use('/api/v1/article', ArticleRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.get('/service-worker', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
+
 app.listen(port, err => {
   if (err) console.log(err);
   else console.log(`Server running on port ${port}`);
